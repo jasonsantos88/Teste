c31934cfd3cf5e7dff139e38c64ecce17d337ea9
Douglas Porto <douglasalexandre7@gmail.com>
2016-04-10 02:29:16 +0000

    Incluindo Blog e Mudando alguns dados

--- js/function.js
+++ js/function.js
@@ -145,6 +145,7 @@ DP.fancyBox = function(){
     }
 }
 
+
 $(document).ready(function(){
     // Call placeholder.js to enable Placeholder Property for IE9
     Modernizr.load([
@@ -164,9 +165,28 @@ $(document).ready(function(){
     }
     ]);
 
-
+    
+    
     DP.filter();
     DP.fancyBox();
+    var data = idade(1990, 01, 31);
+    $("#age").html(data);
 });
-
 });
+function idade(ano_aniversario, mes_aniversario, dia_aniversario) {
+    var d = new Date,
+        ano_atual = d.getFullYear(),
+        mes_atual = d.getMonth() + 1,
+        dia_atual = d.getDate(),
+  
+        ano_aniversario = +ano_aniversario,
+        mes_aniversario = +mes_aniversario,
+        dia_aniversario = +dia_aniversario,
+  
+        quantos_anos = ano_atual - ano_aniversario;
+    if (mes_atual < mes_aniversario || mes_atual == mes_aniversario && dia_atual < dia_aniversario) {
+        quantos_anos--;
+    }
+  
+  return quantos_anos < 0 ? 0 : quantos_anos;
+}
