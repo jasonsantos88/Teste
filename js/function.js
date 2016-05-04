dd61e997d8eb403691fcb5d3402b6213df704516
Douglas Porto <douglasalexandre7@gmail.com>
2016-05-04 12:31:36 +0000

    Filter and aba work

--- js/function.js
+++ js/function.js
@@ -97,3 +97,38 @@ function idade(ano_aniversario, mes_aniversario, dia_aniversario) {
   
   return quantos_anos < 0 ? 0 : quantos_anos;
 }
+
+(function($) {
+
+  'use strict';
+
+  var $filters = $('.filter [data-filter]'),
+    $boxes = $('.boxes [data-category]');
+
+  $filters.on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    
+    $filters.removeClass('active');
+    $this.addClass('active');
+
+    var $filterColor = $this.attr('data-filter');
+
+    if ($filterColor == 'all') {
+      $boxes.removeClass('is-animated')
+        .fadeOut().finish().promise().done(function() {
+          $boxes.each(function(i) {
+            $(this).addClass('is-animated').delay((i++) * 200).fadeIn();
+          });
+        });
+    } else {
+      $boxes.removeClass('is-animated')
+        .fadeOut().finish().promise().done(function() {
+          $boxes.filter('[data-category = "' + $filterColor + '"]').each(function(i) {
+            $(this).addClass('is-animated').delay((i++) * 200).fadeIn();
+          });
+        });
+    }
+  });
+
+})(jQuery);
